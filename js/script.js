e92e8a06e2176c08ace36045699b116331679035
Rhuan Lucas <rhuanfernandes.rf@gmail.com>
2022-05-25 23:37:39 -0300

    Corrigido responsividade

--- js/script.js
+++ js/script.js
@@ -1,24 +1,26 @@
-$('.button-question').click(function (e) { 
-    e.preventDefault();
+$(document).ready(function(){
+    $('.button-question').click(function (e) { 
+        e.preventDefault();
+        
+        $('.button-question i').css('transform', 'rotate(0)');
+        const answer = $(this).next();
+        
+        if(answer.hasClass('closed')){
+            $('.answer').addClass('closed');
+            $('.answer').removeClass('open');
+            
+            answer.addClass('open');
+            answer.removeClass('closed');
+            
+            $(this).find('i').css('transform', 'rotate(45deg)');
+            
+            
+        }
+        else if(answer.hasClass('open')){
+            answer.removeClass('open');
+            answer.addClass('closed');
+            
+        }
     
-    $('.button-question i').css('transform', 'rotate(0)');
-    const answer = $(this).next();
-    
-    if(answer.hasClass('closed')){
-        $('.answer').addClass('closed');
-        $('.answer').removeClass('open');
-        
-        answer.addClass('open');
-        answer.removeClass('closed');
-        
-        $(this).find('i').css('transform', 'rotate(45deg)');
-        
-        
-    }
-    else if(answer.hasClass('open')){
-        answer.removeClass('open');
-        answer.addClass('closed');
-        
-    }
-
+    })
 })
